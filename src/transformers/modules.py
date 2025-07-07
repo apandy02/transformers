kicked off gpt train run with fixed gradient flow

src/transformers/modules.py
@@ -1,9 +1,11 @@
 """
 Implementation of all the sub-modules required to build transformer blocks
 """
-import torch
 import math
+
+import torch
 from torch import nn
+
 
 def scaled_dot_product_attention(q, k, d_k, mask):
     scores = torch.matmul(q, k.transpose(-2, -1)) / math.sqrt(d_k)

src/transformers/transformer_blocks.py
@@ -3,8 +3,11 @@ Implementations of different versions of transformer layers/blocks
 Aryaman Pandya
 """
 
-from modules import MultiHeadAttention, PositionWiseFFN, positional_embedding
 from torch import nn
+
+from transformers.modules import (MultiHeadAttention, PositionWiseFFN,
+                                  positional_embedding)
+
 
 class TransformerLayer(nn.Module):
     """